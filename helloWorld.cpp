1dd12ad74d55572644171d7c689d1014a03650cc
Mrockwell2 <120338765+Mrockwell2@users.noreply.github.com>
2023-01-24 00:00:23 +0000

    deleted:    helloWorld 	modified:   helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void sieve(int n, int* primes) {
+	int temp_primes[n-1];
+	for(int i = 2; i <= n; i++)
+		temp_primes[i-2] = i;
+}
+
 int main() {
 	cout << "Hello World!!" << endl;
+	int num = 10;
+	int primes [9] = { };
+	sieve(num, primes);
+	cout << "Testing array creation:";
+	for(int i = 0; i < (num-1); i++)
+		cout << " " << primes[i];
 }
